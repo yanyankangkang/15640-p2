78db1c9b0f67354abeac931e94c22758f3a6e89c
roddykou <roddykou@gmail.com>
2013-10-04 21:11:37 -0400

    update RMI msg

--- src/RMIMessage.java
+++ src/RMIMessage.java
@@ -1,4 +1,6 @@
 import java.io.Serializable;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 /**
  * RMIMessage class encapsulates a method invocation, it represents the
@@ -7,71 +9,78 @@ import java.io.Serializable;
 
 /**
  * @author yinxu
- *
+ * 
  */
 public class RMIMessage implements Serializable {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 2131510874174393140L;
-	
-	private String methodName;
+
+	// variables need to give to server
 	private RemoteObjectReference ror;
-	//temporary arg format using String
-	private String arg;
-	private Object ret;
-	
-	public RMIMessage(){}
-	public RMIMessage(String methodName, String arg) {
+	private Object callee;
+	private String methodName;
+	private Object[] args;
+	private Class[] argsTypes;
+
+	// variables need to get back from server
+	private Object returnValue;
+	private boolean exceptionThrown = false;
+	private Exception exception = null;
+
+	public RMIMessage(Object callee, String methodName, Object[] args) {
+		this.callee = callee;
 		this.methodName = methodName;
-		this.arg = arg;
+		this.args = args;
+
+		if (args != null) {
+			int argsLength = args.length;
+			this.argsTypes = new Class[argsLength];
+			for (int i = 0; i < argsLength; i++) {
+				this.argsTypes[i] = args[i].getClass();
+			}
+		}
 	}
-	
-	
-	/**
-	 * @return the methodName
-	 */
-	public String getMethodName() {
-		return methodName;
+
+	public Object invokeMethod() {
+		Method method = null;
+		try {
+			method = this.callee.getClass().getMethod(methodName, argsTypes);
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+
+		try {
+			this.returnValue = method.invoke(this.callee, this.args);
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			// e.printStackTrace();
+			this.exceptionThrown = true;
+			this.exception = e;
+			return null;
+		}
+
+		return this.returnValue;
 	}
-	/**
-	 * @param methodName the methodName to set
-	 */
-	public void setMethodName(String methodName) {
-		this.methodName = methodName;
+
+	public boolean ifExceptionThrown() {
+		return this.exceptionThrown;
 	}
-	/**
-	 * @return the ror
-	 */
-	public RemoteObjectReference getRor() {
-		return ror;
+
+	public Exception getException() {
+		return this.exception;
 	}
-	/**
-	 * @param ror the ror to set
-	 */
-	public void setRor(RemoteObjectReference ror) {
-		this.ror = ror;
-	}
-	/**
-	 * @return the arg
-	 */
-	public String getArg() {
-		return arg;
-	}
-	/**
-	 * @param arg the arg to set
-	 */
-	public void setArg(String arg) {
-		this.arg = arg;
-	}
-	public Object getRet() {
-		return ret;
-	}
-	public void setRet(Object ret) {
-		this.ret = ret;
-	}
-	
-	
-	
 }
